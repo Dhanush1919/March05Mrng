82834f513c9886f6939e550117c85da7cac5e9d3
Dhanush1919 <dhanush.venkataraman@gmail.com>
2020-03-05 09:15:36 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+var i=1;
+var fact=1;
+var n=5;
+for(i=n;i>=1;i--)
+{
+  fact=fact*i;
+}
+console.log(fact);
